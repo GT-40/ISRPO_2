d9ed0558845d3524b55cd22a6c03fa2b7bed63a0
Dmitry <dtelepov1610@gmail.com>
2020-12-20 10:19:00 +0300

    v.2 still not working

---
+++ addFunc.h
@@ -0,0 +1,6 @@
+#ifndef _ADDFUNC_H
+#define _ADDFUNC_H
+
+Data addData(int i);
+
+#endif
---
+++ funcs.h
@@ -0,0 +1,13 @@
+#ifdef _FUNCS_H
+#define _FUNCS_H
+char* addDate();
+
+char* addTime();
+
+char* addObjct();
+
+char* addApplicant();
+
+char* addApplication();
+
+#endif
